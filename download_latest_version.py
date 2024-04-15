4c80997a848ad66d99225ecaf99966555addd0a8
Damix <158813145+Damix-hash@users.noreply.github.com>
2024-04-15 19:27:51 +0200

    Update download_latest_version.py

--- download_latest_version.py
+++ download_latest_version.py
@@ -1,8 +1,14 @@
 from ChromedriverDownloader import *
 
 # operating_systems are avaible at their official site.
-# download_chromedriver(path, operating_system, version, bool)
-# if you want user on start to get latest version just use latest_
+# download_chromedriver(path, operating_system, version, overrite)
+
+# PATH || STR = Your chromedriver path folder. Leave empty to put chromedriver to PATH of the script.
+# OPERATING_SYSTEM || STR = Operating system. Leave empty for win32.
+# VERSION || INT = Version of chromedriver.
+# OVERRITE || BOOL = Removes chromedriver.exe if exists. (for updating chromedriver to latest)
+
+# if you want user on start to get latest version just use latest_chromedriver()
 
 version = latest_chromedriver()
 
